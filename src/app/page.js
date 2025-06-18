b5b0ce60ae3f076b78697bc411d03d1a3acb1c88
Hrushikesh Gore <143476153+hrushiK1415@users.noreply.github.com>
2025-06-18 20:48:26 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <>
       <Head>
+    FarmGuard - Empowering Farmers
         <title>FarmGuard - Empowering Farmers</title>
       </Head>
       <GreenGuideLandingPage />
